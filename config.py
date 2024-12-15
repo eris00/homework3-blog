ee3bb352fb2cfd904d9b882adfa935c662fa865e
eris00 <erissutkovic00@gmail.com>
2024-12-15 04:35:02 +0100

    .env separated

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:ebackender44@localhost/blog-project-t1"
-    SECRET_KEY:str = "SECRET_KEY"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    DATABASE_URL: str
+    SECRET_KEY:str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
